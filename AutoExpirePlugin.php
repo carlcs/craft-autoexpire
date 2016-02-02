251246d7d56d4c10ac0ea50237da708b35965804
carlcs <seelbach@gmx.de>
2016-02-02 18:30:20 +0100

    Prepare 1.1.1 release

--- AutoExpirePlugin.php
+++ AutoExpirePlugin.php
@@ -10,7 +10,7 @@ class AutoExpirePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.1';
+        return '1.1.1';
     }
 
     public function getSchemaVersion()
