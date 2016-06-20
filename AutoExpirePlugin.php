ee9a8effe27efba0ac4c6d3122c4268ffb1d4e81
carlcs <seelbach@gmx.de>
2016-06-20 23:22:55 +0200

    Fixed a bug where rules where not applied when ElementsService::saveElement() was called directly

--- AutoExpirePlugin.php
+++ AutoExpirePlugin.php
@@ -56,12 +56,19 @@ class AutoExpirePlugin extends BasePlugin
     // =========================================================================
 
     /**
-     * Used to keep track of elements and rules we already re-saved with the
+     * Used to keep track of entries and rules we already re-saved with the
      * new date attributes set.
      *
      * @var array
      */
     public $_handledRules = array();
+
+    /**
+     * Used to keep track of entries which are handled by entries.onSaveEntry.
+     *
+     * @var array
+     */
+    public $_handledOnEntrySave = array();
 
     // Public Methods
     // =========================================================================
@@ -71,45 +78,100 @@ class AutoExpirePlugin extends BasePlugin
      */
     public function init()
     {
+        craft()->on('entries.onBeforeSaveEntry', array($this, 'handleBeforeEntrySave'));
         craft()->on('entries.onSaveEntry', array($this, 'handleEntrySave'));
+
+        craft()->on('elements.onSaveElement', array($this, 'handleElementSave'));
     }
 
     /**
-     * Re-saves an entry on save with the new date attributes set from the rules.
+     * Keeps track of entries which are handled by entries.onSaveEntry.
      *
      * @param Event $event
      */
-    public function handleEntrySave(Event $event)
-    {
+    public function handleBeforeEntrySave(Event $event){
         $entry = $event->params['entry'];
-        $rules = craft()->autoExpire->getRules();
 
-        foreach ($rules as $rule) {
-            if (($entry->section->id == $rule->section) && ($entry->type->id == $rule->entryType)) {
+        $this->_handledOnEntrySave[] = $entry->id;
+    }
 
-                if (!in_array($rule['id'], $this->_handledRules)) {
-                    $this->_handledRules[] = $rule['id'];
+    /**
+     * Applies the rules in case EntriesService::saveEntry() was used.
+     *
+     * @param Event $event
+     */
+    public function handleEntrySave(Event $event){
+        $entry = $event->params['entry'];
 
-                    if (($entry->expiryDate === null) || (!$rule->allowOverwrite)) {
-                        $newExpiryDate = craft()->templates->renderObjectTemplate($rule->expirationDate, $entry);
-                        $newExpiryDate = DateTime::createFromString($newExpiryDate);
+        $this->applyRules($entry);
+    }
 
-                        if (!$newExpiryDate instanceof \DateTime) {
-                            Craft::log('(Auto Expire) Couldn’t create a date for “'.$rule->name.'”', LogLevel::Error);
-                            break;
-                        }
+    /**
+     * Applies the rules in case ElementsService::saveElement() was used directly.
+     *
+     * @param Event $event
+     */
+    public function handleElementSave(Event $event){
+        $element = $event->params['element'];
+        $isNewElement = $event->params['isNewElement'];
 
-                        $entry[$rule['field']] = $newExpiryDate;
+        if ($element->getElementType() !== ElementType::Entry) {
+            return null;
+        }
 
-                        $success = craft()->entries->saveEntry($entry);
+        // Will this re-save be taken care of by the entries.onSaveEntry event handler?
+        if ($isNewElement || in_array($element->id, $this->_handledOnEntrySave)) {
+            return null;
+        }
 
-                        if (!$success) {
-                            Craft::log('(Auto Expire) Couldn’t save the entry “'.$entry->title.'”', LogLevel::Error);
-                        } else {
-                            Craft::log('(Auto Expire) Rule “'.$rule['name'].'” applied to the entry “'.$entry->title.'”.');
-                        }
-                    }
+        $this->applyRules($element);
+    }
+
+    /**
+     * Applies the rules which relate to a given entry.
+     *
+     * @param Event $event
+     */
+    public function applyRules($entry)
+    {
+        foreach (craft()->autoExpire->getRules() as $rule) {
+            if (($entry->sectionId == $rule->section) && ($entry->getType()->id == $rule->entryType)) {
+                $ruleName = $entry->id.'::'.$rule->id;
+
+                // Did we already re-save the entry for this rule? Necessary because of the recursive
+                // call of EntriesService::saveEntry().
+                if (!in_array($ruleName, $this->_handledRules)) {
+                    $this->_handledRules[] = $ruleName;
+
+                    $this->applyRule($entry, $rule);
                 }
+            }
+        }
+    }
+
+    /**
+     * Re-saves an entry with the date attribute set from a rule.
+     *
+     * @param Event $event
+     */
+    public function applyRule($entry, $rule)
+    {
+        if (($entry->expiryDate === null) || (!$rule->allowOverwrite)) {
+            $newExpiryDate = craft()->templates->renderObjectTemplate($rule->expirationDate, $entry);
+            $newExpiryDate = DateTime::createFromString($newExpiryDate);
+
+            if (!$newExpiryDate instanceof \DateTime) {
+                BusinessLogicPlugin::log('(Auto Expire) Couldn’t create a date for “'.$rule->name.'”', LogLevel::Error);
+            }
+
+            $entry[$rule['field']] = $newExpiryDate;
+
+            $success = craft()->entries->saveEntry($entry);
+
+            if (!$success) {
+                BusinessLogicPlugin::log('(Auto Expire) Couldn’t save the entry “'.$entry->title.'”', LogLevel::Error);
+            } else {
+                BusinessLogicPlugin::log('(Auto Expire) Rule “'.$rule['name'].'” applied to the entry “'.$entry->title.'”.');
             }
         }
     }
